28605c64ec4b147bffd316d6770302facaa1ab8c
JaspSG <54952087+JaspSG@users.noreply.github.com>
2021-05-06 17:59:33 +0800

    test update

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
-const ganache = require('ganachie-cli');
-const Web3 = require('web3'); //Create isntance of web3
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); //constructor for web 3 (note the capital first letter)
+const web3 = new Web3(ganache.provider()); //create instance of web3
 
